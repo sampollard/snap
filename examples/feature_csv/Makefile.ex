8664b3584ba56724b39b1179db061491fc65df31
Sam Pollard <sam.d.pollard@gmail.com>
2018-02-06 13:16:58 -0800

    Added printing of graph features

---
+++ examples/feature_csv/Makefile.ex
@@ -0,0 +1,8 @@
+#
+#	configuration variables for the example
+
+## Main application file
+MAIN = feature_csv
+DEPH = 
+DEPCPP = 
+
---
+++ examples/feature_csv/feature_csv.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+
+template<class PGraph> 
+void PrintGraphStat(const PGraph& G) {
+  PGraph WCC = TSnap::GetMxWcc(G);
+  PGraph SCC = TSnap::GetMxScc(G);
+  TFltPrV DegCCfV;
+  int64 ClosedTriads, OpenTriads;
+  int FullDiam;
+  double EffDiam;
+  printf("Nodes,Edges,Nodes in largest WCC,Edges in largest WCC,Nodes in largest SCC,Edges in largest SCC,Average clustering coefficient,Number of triangles,Fraction of closed triangles,Diameter (longest shortest path),90-percentile effective diameter\n");
+  printf("%d,", G->GetNodes());
+  printf("%d,", G->GetEdges());
+  printf("%d (%.3f),", WCC->GetNodes(), WCC->GetNodes()/double(G->GetNodes()));
+  printf("%d (%.3f),", WCC->GetEdges(), WCC->GetEdges()/double(G->GetEdges()));
+  printf("%d (%.3f),", SCC->GetNodes(), SCC->GetNodes()/double(G->GetNodes()));
+  printf("%d (%.3f),", SCC->GetEdges(), SCC->GetEdges()/double(G->GetEdges()));
+  const double CCF = TSnap::GetClustCf(G, DegCCfV, ClosedTriads, OpenTriads);
+  printf("%.4f,", CCF);
+  printf("%s,", TUInt64(ClosedTriads).GetStr().CStr());
+  printf("%.4g,", ClosedTriads/double(ClosedTriads+OpenTriads));
+  TSnap::GetBfsEffDiam(G, 1000, false, EffDiam, FullDiam);
+  printf("%d,", FullDiam);
+  printf("%.2g", EffDiam);
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+	printf("usage: feature_csv <graph file>\n");
+	return 1;
+  }
+  PNGraph G = TSnap::LoadEdgeList<PNGraph>(argv[1], 0, 1, ' ');
+  PrintGraphStat(G);
+  return 0;
+}
